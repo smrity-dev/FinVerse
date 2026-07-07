4331859619321f2c88181f054f2c23baea990d35
Smrity <smritykumari7519@gmail.com>
2026-07-07 15:56:16 +0530

    Transaction saves concepts

--- src/com/finverse/dao/TransactionDAO.java
+++ src/com/finverse/dao/TransactionDAO.java
@@ -4,7 +4,7 @@ import com.finverse.model.Transaction;
 import java.util.List;
 
 public interface TransactionDAO {
-    
+
     void saveTransaction(Transaction transaction);
     List<Transaction> getTransactions(int accountId);
 
