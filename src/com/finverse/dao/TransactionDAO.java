9725681acda62792aefd8289c5c967a2962ff18d
Smrity <smritykumari7519@gmail.com>
2026-07-07 15:11:54 +0530

    Create TransactionDAO interface

---
+++ src/com/finverse/dao/TransactionDAO.java
@@ -0,0 +1,11 @@
+package com.finverse.dao;
+
+import com.finverse.model.Transaction;
+import java.util.List;
+
+public interface TransactionDAO {
+    
+    void saveTransaction(Transaction transaction);
+    List<Transaction> getTransactions(int accountId);
+
+}
