943945ac7b0219b2be09bdd583d2ef413cbbcf74
Smrity <smritykumari7519@gmail.com>
2026-07-07 15:09:28 +0530

    Added Transaction model

---
+++ src/com/finverse/model/Transaction.java
@@ -0,0 +1,95 @@
+package com.finverse.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    // Declare private variables
+    private int transactionId;
+    private String transactionType;
+    private BigDecimal amount;
+    private BigDecimal balanceAfterTransaction;
+    private String remarks;
+    private LocalDateTime transactionTime;
+    private int accountId;
+
+    //Default Constructor
+    public Transaction() {
+    }
+
+    // Generate  Getter-Setter method
+    public int getTransactionId() {
+        return transactionId;
+    }
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getBalanceAfterTransaction() {
+        return balanceAfterTransaction;
+    }
+    public void setBalanceAfterTransaction(BigDecimal balanceAfterTransaction) {
+        this.balanceAfterTransaction = balanceAfterTransaction;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public LocalDateTime getTransactionTime() {
+        return transactionTime;
+    }
+    public void setTransactionTime(LocalDateTime transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    // Generate Constructor
+    public Transaction(int transactionId, String transactionType, BigDecimal amount, BigDecimal balanceAfterTransaction, String remarks, LocalDateTime transactionTime, int accountId) {
+        this.transactionId = transactionId;
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.balanceAfterTransaction = balanceAfterTransaction;
+        this.remarks = remarks;
+        this.transactionTime = transactionTime;
+        this.accountId = accountId;
+    }
+
+    // Generate toString()
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", transactionType='" + transactionType + '\'' +
+                ", amount=" + amount +
+                ", balanceAfterTransaction=" + balanceAfterTransaction +
+                ", remarks='" + remarks + '\'' +
+                ", transactionTime=" + transactionTime +
+                ", accountId=" + accountId +
+                '}';
+    }
+}
