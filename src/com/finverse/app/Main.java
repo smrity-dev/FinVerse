0afb67998ca3043aff36c6b7ed7d7dc2dd11346a
Smrity <smritykumari7519@gmail.com>
2026-07-07 14:12:23 +0530

    Add Application entry point

---
+++ src/com/finverse/app/Main.java
@@ -0,0 +1,11 @@
+package com.finverse.app;
+
+import com.finverse.ui.BankingUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankingUI bankingUI = new BankingUI();
+        bankingUI.start();
+    }
+}
