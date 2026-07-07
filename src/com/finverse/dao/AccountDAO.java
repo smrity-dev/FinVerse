04c96da3bbae59bccad2554cbd6022ae39ddc8ae
Smrity <smritykumari7519@gmail.com>
2026-07-07 15:59:40 +0530

    Implementation of AccountDAO

--- src/com/finverse/dao/AccountDAO.java
+++ src/com/finverse/dao/AccountDAO.java
@@ -2,9 +2,15 @@ package com.finverse.dao;
 
 import com.finverse.model.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
 
     void saveAccount(Account account);
     Account getAccountByUserId(int userId);
+
+    void updateAccount(Account account);
+    Account getAccountByAccountNumber(String accountNumber);
+    List<Account> getAllAccounts();
 
 }
--- src/com/finverse/dao/AccountDAOImpl.java
+++ src/com/finverse/dao/AccountDAOImpl.java
@@ -4,12 +4,15 @@ import com.finverse.model.Account;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class AccountDAOImpl implements AccountDAO {
     private static final List<Account> accounts = new ArrayList<>();
+
     @Override
     public void saveAccount(Account account) {
         accounts.add(account);
     }
+
     @Override
     public Account getAccountByUserId(int userId) {
         for (Account account : accounts) {
@@ -18,5 +21,25 @@ public class AccountDAOImpl implements AccountDAO {
             }
         }
         return null;
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        // ArrayList me object reference update ho jata hai.
+        // JDBC version me yahi SQL UPDATE banega.
+    }
+
+    @Override
+    public Account getAccountByAccountNumber(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
+    @Override
+    public List<Account> getAllAccounts() {
+        return accounts;
     }
 }
