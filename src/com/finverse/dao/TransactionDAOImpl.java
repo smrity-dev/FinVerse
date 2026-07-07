3c40c1ab0293f80b304743b4b78ebe9f855e2b50
Smrity <smritykumari7519@gmail.com>
2026-07-07 15:57:04 +0530

    Implementations of Transaction

---
+++ src/com/finverse/dao/TransactionDAOImpl.java
@@ -0,0 +1,26 @@
+package com.finverse.dao;
+
+import com.finverse.model.Transaction;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionDAOImpl implements TransactionDAO {
+
+    private static final List<Transaction> transactions = new ArrayList<>();
+
+    @Override
+    public void saveTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    @Override
+    public List<Transaction> getTransactions(int accountId) {
+        List<Transaction> list = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction.getAccountId() == accountId) {
+                list.add(transaction);
+            }
+        }
+        return list;
+    }
+}
