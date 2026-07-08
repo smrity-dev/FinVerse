5a9d26e191954326c83e68230d6e9c96b95eb374
Smrity <smritykumari7519@gmail.com>
2026-07-08 14:29:03 +0530

    Restrictions on invalid email,phone & Passwords

--- src/com/finverse/validation/UserValidation.java
+++ src/com/finverse/validation/UserValidation.java
@@ -11,7 +11,7 @@ public class UserValidation {
     }
 
     public static boolean isValidPassword(String password) {
-        return password.length() >= 8;
+        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!_*()\\-]).{8,}$");
     }
 
 }
