bc9639bfc1ed29c921a1a2529b78b5372e8314a6
Smrity <smritykumari7519@gmail.com>
2026-07-10 20:02:50 +0530

    Implementation of UserDAO interface

--- src/com/finverse/dao/UserDAOImpl.java
+++ src/com/finverse/dao/UserDAOImpl.java
@@ -4,6 +4,7 @@ import com.finverse.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
+// UserDAO interface me methods likha hai usi ko define kara hai idher
 public class UserDAOImpl implements UserDAO {
     private static final List<User> users = new ArrayList<>();
 
@@ -27,19 +28,38 @@ public class UserDAOImpl implements UserDAO {
         public boolean emailExists(String email) {
             for (User user : users) {
                 if (user.getEmail().equalsIgnoreCase(email)) {
+                    //equalsIgnoreCase capital small letters ko ignore kar ke compare kare ga ex:-  Smrity and smrity dono same rhega
                     return true;
                 }
             }
             return false;
         }
 
+
     @Override
     public boolean phoneExists(String phoneNumber) {
         for (User user : users) {
             if (user.getPhoneNumber().equals(phoneNumber)) {
+                //equals case sensitivite hota hai exactly same words hone chaiye tabhi true milega ex:- Smrity ka Smrity hi not smrity
                 return true;
             }
         }
         return false;
+    }
+
+    // Password change,Forget ke liye 2 methods
+    @Override
+    public void updateUser(User user) {
+        // ArrayList me object automatically update ho jata hai.
+        // JDBC version me SQL UPDATE chalega.
+    }
+    @Override
+    public User getUserByEmail(String email) {
+        for (User user : users) {
+            if (user.getEmail().equalsIgnoreCase(email)) {
+                return user;
+            }
+        }
+        return null;
     }
 }
