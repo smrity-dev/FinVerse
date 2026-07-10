974abdc27d54681014223540af1d7f39fa134d02
Smrity <smritykumari7519@gmail.com>
2026-07-10 20:01:25 +0530

    Password stored method in UserDAO

--- src/com/finverse/dao/UserDAO.java
+++ src/com/finverse/dao/UserDAO.java
@@ -6,7 +6,13 @@ public interface UserDAO {
 
     void saveUser(User user);
     User login(String email, String password);
-    
+
     boolean emailExists(String email);
     boolean phoneExists(String phoneNumber);
+
+    // Password change,forget karne ke liye
+    void updateUser(User user);
+    User getUserByEmail(String email);
 }
+
+// UserDAO Database store rakhta hai , ye ek interface class hai , inka andar ke methods ko UserDAOImpl implement karega alag file me
