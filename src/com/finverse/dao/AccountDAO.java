147b8c76b4683f8a396fda2561ad9cfbd4bb5953
Smrity <smritykumari7519@gmail.com>
2026-07-15 13:59:35 +0530

    Add Methods of delete,close,activate,change account to implement

--- src/com/finverse/dao/AccountDAO.java
+++ src/com/finverse/dao/AccountDAO.java
@@ -1,8 +1,9 @@
 package com.finverse.dao;
 
 import com.finverse.model.Account;
-
 import java.util.List;
+import java.math.BigDecimal;
+import com.finverse.model.AccountType;
 
 public interface AccountDAO {
 
@@ -12,5 +13,15 @@ public interface AccountDAO {
     void updateAccount(Account account);
     Account getAccountByAccountNumber(String accountNumber);
     List<Account> getAllAccounts();
+
+    void deleteAccount(Account account);
+    void blockAccount(Account account);
+    void activateAccount(Account account);
+    void closeAccount(Account account);
+    void changeAccountType(Account account, AccountType accountType);
+    BigDecimal getTotalBalance();
+    int getTotalAccounts();
+    int getActiveAccounts();
+    int getClosedAccounts();
 
 }
