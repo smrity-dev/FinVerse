954ba5195c4fcd4c5d32e6dc3991086c1ba42003
Smrity <smritykumari7519@gmail.com>
2026-07-15 14:10:12 +0530

    Implement methods of AccountDAO

--- src/com/finverse/dao/AccountDAOImpl.java
+++ src/com/finverse/dao/AccountDAOImpl.java
@@ -1,6 +1,10 @@
 package com.finverse.dao;
 
 import com.finverse.model.Account;
+import com.finverse.model.AccountStatus;
+import com.finverse.model.AccountType;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,5 +45,66 @@ public class AccountDAOImpl implements AccountDAO {
     @Override
     public List<Account> getAllAccounts() {
         return accounts;
+    }
+
+    @Override
+    public void deleteAccount(Account account) {
+        accounts.remove(account);
+    }
+
+    @Override
+    public void blockAccount(Account account) {
+        account.setAccountStatus(AccountStatus.BLOCKED);
+    }
+
+    @Override
+    public void activateAccount(Account account) {
+        account.setAccountStatus(AccountStatus.ACTIVE);
+    }
+
+    @Override
+    public void closeAccount(Account account) {
+        account.setAccountStatus(AccountStatus.CLOSED);
+    }
+
+    @Override
+    public void changeAccountType(Account account, AccountType accountType) {
+        account.setAccountType(accountType);
+    }
+
+    @Override
+    public BigDecimal getTotalBalance() {
+        BigDecimal total = BigDecimal.ZERO;
+        for(Account account : accounts){
+            total = total.add(account.getBalance());
+        }
+        return total;
+    }
+
+    @Override
+    public int getTotalAccounts() {
+        return accounts.size();
+    }
+
+    @Override
+    public int getActiveAccounts(){
+        int count=0;
+        for(Account account:accounts){
+            if(account.getAccountStatus()==AccountStatus.ACTIVE){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public int getClosedAccounts(){
+        int count=0;
+        for(Account account:accounts){
+            if(account.getAccountStatus()==AccountStatus.CLOSED){
+                count++;
+            }
+        }
+        return count;
     }
 }
