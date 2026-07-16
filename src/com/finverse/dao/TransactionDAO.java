0367ee7570fa7461cedca1ed9bae56e454fe58ed
Smrity <smritykumari7519@gmail.com>
2026-07-16 17:28:42 +0530

    Transactions Connected to Database

--- src/com/finverse/dao/TransactionDAO.java
+++ src/com/finverse/dao/TransactionDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface TransactionDAO {
 
     void saveTransaction(Transaction transaction);
-    List<Transaction> getTransactions(int accountId);
+    List<Transaction> getTransactions(String accountNumber);
 
     List<Transaction> getAllTransactions();
     List<Transaction> getTransactions();
--- src/com/finverse/dao/TransactionDAOImpl.java
+++ src/com/finverse/dao/TransactionDAOImpl.java
@@ -4,35 +4,145 @@ import com.finverse.model.Transaction;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TransactionDAOImpl implements TransactionDAO {
+// Database connection Imports
 
-    private static final List<Transaction> transactions = new ArrayList<>();
+import com.finverse.database.DBConnection;
+import com.finverse.model.TransactionStatus;
+import com.finverse.model.TransactionType;
+import java.sql.*;
+
+public class TransactionDAOImpl implements TransactionDAO {
 
     @Override
     public void saveTransaction(Transaction transaction) {
-        transactions.add(transaction);
+
+        String sql = """
+            INSERT INTO transactions
+            (
+                transaction_id,
+                account_number,
+                transaction_type,
+                transaction_status,
+                amount,
+                balance_after_transaction,
+                reference_number,
+                description,
+                created_at
+            )
+            VALUES(?,?,?,?,?,?,?,?,?)
+            """;
+
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql)){
+
+            ps.setInt(1, transaction.getTransactionId());
+            ps.setString(2, transaction.getAccountNumber());
+            ps.setString(3, transaction.getTransactionType().name());
+            ps.setString(4, transaction.getTransactionStatus().name());
+            ps.setBigDecimal(5, transaction.getAmount());
+            ps.setBigDecimal(6, transaction.getBalanceAfterTransaction());
+            ps.setString(7, transaction.getReferenceNumber());
+            ps.setString(8, transaction.getRemarks());
+
+            ps.setTimestamp(
+                    9,
+                    Timestamp.valueOf(transaction.getTransactionTime())
+            );
+
+            ps.executeUpdate();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public List<Transaction> getTransactions(int accountId) {
+    public List<Transaction> getTransactions(String accountNumber) {
+
         List<Transaction> list = new ArrayList<>();
-        for (Transaction transaction : transactions) {
-            if (transaction.getAccountId() == accountId) {
+        String sql = "SELECT * FROM transactions WHERE account_number=? ORDER BY created_at DESC";
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setString(1,accountNumber);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Transaction transaction = new Transaction();
+                transaction.setTransactionId(
+                        rs.getInt("transaction_id"));
+                transaction.setAccountNumber(
+                        rs.getString("account_number"));
+
+                transaction.setTransactionType(
+                        TransactionType.valueOf(
+                                rs.getString("transaction_type")));
+                transaction.setTransactionStatus(
+                        TransactionStatus.valueOf(
+                                rs.getString("transaction_status")));
+                transaction.setAmount(
+                        rs.getBigDecimal("amount"));
+                transaction.setBalanceAfterTransaction(
+                        rs.getBigDecimal("balance_after_transaction"));
+                transaction.setReferenceNumber(
+                        rs.getString("reference_number"));
+                transaction.setRemarks(
+                        rs.getString("description"));
+                transaction.setTransactionTime(
+                        rs.getTimestamp("created_at")
+                                .toLocalDateTime());
                 list.add(transaction);
             }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
         }
         return list;
     }
 
     // User
     @Override
-    public List<Transaction> getAllTransactions(){
-        return transactions;
+    public List<Transaction> getAllTransactions() {
+        List<Transaction> list = new ArrayList<>();
+        String sql = "SELECT * FROM transactions ORDER BY created_at DESC";
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery())
+        {
+            while(rs.next()){
+                Transaction transaction = new Transaction();
+                transaction.setTransactionId(
+                        rs.getInt("transaction_id"));
+                transaction.setAccountNumber(
+                        rs.getString("account_number"));
+                transaction.setTransactionType(
+                        TransactionType.valueOf(
+                                rs.getString("transaction_type")));
+                transaction.setTransactionStatus(
+                        TransactionStatus.valueOf(
+                                rs.getString("transaction_status")));
+                transaction.setAmount(
+                        rs.getBigDecimal("amount"));
+                transaction.setBalanceAfterTransaction(
+                        rs.getBigDecimal("balance_after_transaction"));
+                transaction.setReferenceNumber(
+                        rs.getString("reference_number"));
+                transaction.setRemarks(
+                        rs.getString("description"));
+                transaction.setTransactionTime(
+                        rs.getTimestamp("created_at")
+                                .toLocalDateTime());
+                list.add(transaction);
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return list;
     }
 
     // Admin
     @Override
     public List<Transaction> getTransactions() {
-        return transactions;
+        return getAllTransactions();
     }
 }
