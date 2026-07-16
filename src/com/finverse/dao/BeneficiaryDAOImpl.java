4471c60ccd8aaa6b6e1e8989a6f12bd09b2d497a
Smrity <smritykumari7519@gmail.com>
2026-07-16 19:59:11 +0530

    Beneficiary connected to Database

--- src/com/finverse/dao/BeneficiaryDAOImpl.java
+++ src/com/finverse/dao/BeneficiaryDAOImpl.java
@@ -4,40 +4,124 @@ import com.finverse.model.Beneficiary;
 import java.util.ArrayList;
 import java.util.List;
 
+// Database connection Imports
+
+import com.finverse.database.DBConnection;
+import java.sql.*;
+
 public class BeneficiaryDAOImpl implements BeneficiaryDAO{
 
     private static final List<Beneficiary> beneficiaries=new ArrayList<>();
 
     @Override
     public void saveBeneficiary(Beneficiary beneficiary) {
-        beneficiaries.add(beneficiary);
+
+        String sql = """
+            INSERT INTO beneficiaries
+            (
+                user_id,
+                beneficiary_name,
+                account_number,
+                added_at
+            )
+            VALUES (?,?,?,?)
+            """;
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, beneficiary.getUserId());
+            ps.setString(2, beneficiary.getBeneficiaryName());
+            ps.setString(3, beneficiary.getAccountNumber());
+            ps.setTimestamp(4,
+                    Timestamp.valueOf(beneficiary.getAddedAt()));
+            ps.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Beneficiary> getBeneficiaries(int userId) {
-        List<Beneficiary> list=new ArrayList<>();
-        for(Beneficiary b:beneficiaries){
-            if(b.getUserId()==userId){
-                list.add(b);
+
+        List<Beneficiary> list = new ArrayList<>();
+        String sql = "SELECT * FROM beneficiaries WHERE user_id=?";
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, userId);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Beneficiary beneficiary = new Beneficiary();
+                beneficiary.setBeneficiaryId(
+                        rs.getInt("beneficiary_id"));
+                beneficiary.setUserId(
+                        rs.getInt("user_id"));
+                beneficiary.setBeneficiaryName(
+                        rs.getString("beneficiary_name"));
+                beneficiary.setAccountNumber(
+                        rs.getString("account_number"));
+                beneficiary.setAddedAt(
+                        rs.getTimestamp("added_at")
+                                .toLocalDateTime());
+                list.add(beneficiary);
             }
+        } catch(SQLException e){
+            e.printStackTrace();
         }
         return list;
     }
 
     @Override
-    public Beneficiary getBeneficiary(int userId,String accountNumber) {
-        for(Beneficiary b:beneficiaries){
-            if(b.getUserId()==userId &&
-                    b.getAccountNumber().equals(accountNumber)){
-                return b;
+    public Beneficiary getBeneficiary(int userId, String accountNumber) {
+
+        String sql = """
+            SELECT *
+            FROM beneficiaries
+            WHERE user_id=?
+            AND account_number=?
+            """;
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, userId);
+            ps.setString(2, accountNumber);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                Beneficiary beneficiary = new Beneficiary();
+                beneficiary.setBeneficiaryId(
+                        rs.getInt("beneficiary_id"));
+                beneficiary.setUserId(
+                        rs.getInt("user_id"));
+                beneficiary.setBeneficiaryName(
+                        rs.getString("beneficiary_name"));
+                beneficiary.setAccountNumber(
+                        rs.getString("account_number"));
+                beneficiary.setAddedAt(
+                        rs.getTimestamp("added_at")
+                                .toLocalDateTime());
+                return beneficiary;
             }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
         }
         return null;
     }
 
     @Override
     public void deleteBeneficiary(Beneficiary beneficiary) {
-        beneficiaries.remove(beneficiary);
+
+        String sql = "DELETE FROM beneficiaries WHERE beneficiary_id=?";
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, beneficiary.getBeneficiaryId());
+            ps.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -46,12 +130,7 @@ public class BeneficiaryDAOImpl implements BeneficiaryDAO{
     }
 
     @Override
-    public Beneficiary searchBeneficiary(int userId,String accountNumber){
-        for(Beneficiary beneficiary:beneficiaries){
-            if(beneficiary.getUserId()==userId
-                    && beneficiary.getAccountNumber().equals(accountNumber))
-                return beneficiary;
-        }
-        return null;
+    public Beneficiary searchBeneficiary(int userId, String accountNumber) {
+        return getBeneficiary(userId, accountNumber);
     }
 }
