2d574c785f2f46fa46070d70458dda0979484749
Smrity <smritykumari7519@gmail.com>
2026-07-17 19:50:05 +0530

    Admin Database connection

--- src/com/finverse/dao/AdminDAO.java
+++ src/com/finverse/dao/AdminDAO.java
@@ -3,4 +3,6 @@ package com.finverse.dao;
 public interface AdminDAO {
 
     boolean login(String username, String password);
+
+
 }
--- src/com/finverse/dao/AdminDAOImpl.java
+++ src/com/finverse/dao/AdminDAOImpl.java
@@ -1,13 +1,38 @@
 package com.finverse.dao;
 
-public class AdminDAOImpl implements AdminDAO{
+import com.finverse.database.DBConnection;
 
-    private final String username="admin";
-    private final String password="admin123";
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminDAOImpl implements AdminDAO {
 
     @Override
-    public boolean login(String username, String password)
-    {
-        return this.username.equals(username) && this.password.equals(password);
+    public boolean login(String username, String password) {
+
+        String sql =
+                "SELECT * FROM admins WHERE username=? AND password=?";
+
+        try (
+                Connection con = DBConnection.getConnection();
+                PreparedStatement ps =
+                        con.prepareStatement(sql)
+        ) {
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            return rs.next();
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
